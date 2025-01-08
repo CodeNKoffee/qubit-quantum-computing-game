6309d6c91c4bdd178122449b9b09d90d144566f9
Hatem Soliman <hatemthedev@gmail.com>
2025-01-08 05:09:20 +0200

    Updated toml

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'firebase', '@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  },
   server: {
-    hmr: {
-      overlay: false // disable error overlay if you find it distracting
-    }
+    port: 3000
   }
 })
